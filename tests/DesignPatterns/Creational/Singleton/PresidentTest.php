357b78a81cd941c082f759e49bd45c8599c2838e
Walmir Silva <walmir.silva@growthdev.com.br>
2023-09-04 15:23:31 -0300

    ajust tests

--- tests/DesignPatterns/Creational/Singleton/PresidentTest.php
+++ tests/DesignPatterns/Creational/Singleton/PresidentTest.php
@@ -9,27 +9,27 @@ use PHPUnit\Framework\TestCase;
 
 final class PresidentTest extends TestCase
 {
-    public function testShouldBeTheSameInstanceForTwoObjets(): void
+    public function testInstancesShouldBeIdentical(): void
     {
         $firstInstance = President::getInstance();
         $secondInstance = President::getInstance();
         $this->assertSame($firstInstance, $secondInstance);
     }
 
-    public function testShouldThrowErrorWhenTryToCreateInstance(): void
+    public function testInstantiationShouldThrowError(): void
     {
         $this->expectException(Error::class);
         $instance = new President();
     }
 
-    public function testShouldThrowErrorWhenTryToClone(): void
+    public function testCloningShouldThrowError(): void
     {
         $this->expectException(Error::class);
         $instance = President::getInstance();
         $clone = clone $instance;
     }
 
-    public function testShouldThrowExceptionWhenTryToSerialize(): void
+    public function testSerializationShouldThrowLogicException(): void
     {
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage('Cannot serialize a President.');
@@ -37,7 +37,7 @@ final class PresidentTest extends TestCase
         $serialize = serialize($instance);
     }
 
-    public function testShouldThrowExceptionWhenTryToUnserialize(): void
+    public function testUnserializationShouldThrowLogicException(): void
     {
         $this->expectException(LogicException::class);
         $instance = President::getInstance();
